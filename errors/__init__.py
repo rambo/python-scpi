f688ec1466a7c0d44df6fa38763a3565db0ddc63
Eero af Heurlin <eero.afheurlin@iki.fi>
2014-02-02 13:59:58 +0200

    forgot to add this

---
+++ errors/__init__.py
@@ -0,0 +1,23 @@
+
+from exceptions import RuntimeError
+
+
+class TimeoutError(RuntimeError):
+    def __init__(self, command, time, *args, **kwargs):
+        self.command = command
+        self.time = time
+        super(TimeoutError, self).__init__(str(self), *args, **kwargs)
+
+    def __str__(self):
+        return "'%s' timed out after %f seconds" % (self.command, self.time)
+
+class CommandError(RuntimeError):
+    def __init__(self, command, code, message, *args, **kwargs):
+        self.command = command
+        self.code = code
+        self.message = message
+        super(CommandError, self).__init__(str(self), *args, **kwargs)
+
+    def __str__(self):
+        return "'%s' returned error %d: %s" % (self.command, self.code, self.message)
+

612a2b85a57c4e602d82edd5fe56ed0b9805e430
Eero af heurlin <eero.afheurlin@iki.fi>
2014-09-03 18:39:52 +0300

    packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+import subprocess
+
+git_version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
+
+setup(
+    name='scpi',
+    version='0.5.dev-%s' % git_version,
+    author='Eero "rambo" af Heurlin',
+    author_email='rambo@iki.fi',
+    packages=['scpi',],
+    license='GNU LGPL',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'pyserial>=2.0',
+    ],
+    url='https://github.com/rambo/python-scpi',
+)
+

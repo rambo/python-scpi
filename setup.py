c96fe7c37c1e09b0dffa388359839640bb41595c
Eero af Heurlin <eero.afheurlin@iki.fi>
2018-07-10 19:28:59 +0300

    Start rewriting for asyncio

--- setup.py
+++ setup.py
@@ -1,25 +1,27 @@
 from distutils.core import setup
 import subprocess
+import sys
+
+if sys.version_info < (3,5):
+    raise RuntimeError("Minimum version python 3.5")
 
 git_version = 'UNKNOWN'
 try:
     git_version = str(subprocess.check_output(['git', 'rev-parse', '--verify', '--short', 'HEAD'])).strip()
-except subprocess.CalledProcessError,e:
-    #print "Got error when trying to read git version: %s" % e
+except subprocess.CalledProcessError as e:
+    #print("Got error when trying to read git version: %s" % e)
     pass
 
 setup(
     name='scpi',
-    version='0.6.6dev-%s' % git_version,
-    #version='0.6.6',
+    version='2.0.0dev-%s' % git_version,
+    #version='2.0.0',
     author='Eero "rambo" af Heurlin',
     author_email='rambo@iki.fi',
     packages=[ 'scpi', 'scpi.errors', 'scpi.transports', 'scpi.devices' ],
     license='GNU LGPL',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', 'rt', encoding='utf-8').read(),
     description='Implement SCPI in pure Python',
-    install_requires=[
-        'pyserial>=2.7',
-    ],
+    install_requires=open('requirements.txt', 'rt', encoding='utf-8').readlines(),
     url='https://github.com/rambo/python-scpi',
 )

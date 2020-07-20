53b0f8dbf99e51960b898ad2ba526484e610c784
MIDE\cflanigan <cflanigan@mide.com>
2020-07-20 13:52:08 -0400

    add serial option for TDKLambda

--- scpi/devices/TDKLambdaZPlus.py
+++ scpi/devices/TDKLambdaZPlus.py
@@ -13,7 +13,27 @@ def tcp(ip, port):
     """Quick helper to connect via TCP"""
     from ..transports.tcp import get as get_tcp
     from ..scpi import SCPIProtocol
-    transport = get_tcp(ip,port)
+    transport = get_tcp(ip, port)
+    protocol = SCPIProtocol(transport)
+    dev = TDKLambdaZplus(protocol)
+    return dev
+
+
+def rs232(port, baudrate=9600):
+    """ Quick helper to connect via serial """
+    from ..transports.rs232 import RS232Transport
+    from ..scpi import SCPIProtocol
+    import serial
+    port = serial.Serial(port,
+                         baudrate=baudrate,
+                         bytesize=8,
+                         parity=serial.PARITY_NONE,
+                         stopbits=1,
+                         xonxoff=False,
+                         rtscts=False,
+                         dsrdtr=False,
+                         timeout=10)
+    transport = RS232Transport(port)
     protocol = SCPIProtocol(transport)
     dev = TDKLambdaZplus(protocol)
     return dev

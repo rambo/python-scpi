925064415a19889a724cfd0adfd78733f0a9a17a
Eero af Heurlin <eero.afheurlin@iki.fi>
2021-03-27 14:38:30 +0200

    use asyncios native timeout system

--- src/scpi/transports/gpib/prologix.py
+++ src/scpi/transports/gpib/prologix.py
@@ -5,7 +5,6 @@ import threading
 
 import serial
 import serial.threaded
-from async_timeout import timeout
 
 from ..rs232 import RS232SerialProtocol
 from .base import GPIBTransport
@@ -64,7 +63,9 @@ class PrologixGPIBTransport(GPIBTransport):
     async def send_and_read(self, send):
         """Send a line, read the response. NOTE: This is for talking with the controller, device responses
         need to use get_response as usual"""
-        with timeout(READ_TIMEOUT):
+
+        async def _send_and_read(send) -> str:
+            """Wrap the actual work"""
             async with self.lock:
                 response = None
 
@@ -79,6 +80,8 @@ class PrologixGPIBTransport(GPIBTransport):
                 self.serialhandler.protocol.write_line(send)
                 await asyncio.get_event_loop().run_in_executor(None, self.blevent.wait)
                 return response
+
+        return await asyncio.wait_for(_send_and_read(send), timeout=READ_TIMEOUT)
 
     async def set_address(self, address, secondary=None):
         """Set the address we want to talk to"""
@@ -148,13 +151,18 @@ class PrologixGPIBTransport(GPIBTransport):
         prev_read_tmo_ms = await self.send_and_read("++read_tmo_ms")
         self.serialhandler.protocol.write_line("++read_tmo_ms %d" % int((SCAN_DEVICE_TIMEOUT / 2) * 1000))
         for addr in range(0, 31):  # 0-30 inclusive
-            with timeout(SCAN_DEVICE_TIMEOUT):
-                try:
-                    await self.set_address(addr)
-                    await self.poll()
-                    found_addresses.append(addr)
-                except (asyncio.TimeoutError, asyncio.CancelledError):
-                    pass
+
+            async def _scan_addr(addr) -> None:
+                """Sacn single address"""
+                nonlocal found_addresses
+                await self.set_address(addr)
+                await self.poll()
+                found_addresses.append(addr)
+
+            try:
+                await asyncio.wait_for(_scan_addr(addr), timeout=SCAN_DEVICE_TIMEOUT)
+            except (asyncio.TimeoutError, asyncio.CancelledError):
+                pass
         self.serialhandler.protocol.write_line("++read_tmo_ms " + prev_read_tmo_ms)
         # Wait a moment for things to settle
         await asyncio.sleep(float(prev_read_tmo_ms) / 1000)

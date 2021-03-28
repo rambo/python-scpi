5d4c580e3309b56e3fde53fb44abf0f8e0a04f2c
Eero af Heurlin <eero.afheurlin@iki.fi>
2021-03-28 13:04:57 +0300

    version bump

--- src/scpi/__init__.py
+++ src/scpi/__init__.py
@@ -1,5 +1,5 @@
 """SCPI module, the scpi class implements the base command set, devices may extend it.
 transports are separate from devices (so you can use for example hp6632b with either serial port or GPIB)"""
-__version__ = "2.2.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
+__version__ = "2.3.0"  # NOTE Use `bump2version --config-file patch` to bump versions correctly
 from .scpi import SCPIProtocol, SCPIDevice
 from .errors import CommandError

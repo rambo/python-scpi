d412a9918fd8831f26480ef66d76c285560c1db3
Eero af Heurlin <eero.afheurlin@iki.fi>
2021-04-10 12:46:23 +0300

    resolve the devices I happen to have to test with into their correct subclasses in this example

--- examples/prologix_usb.py
+++ examples/prologix_usb.py
@@ -7,6 +7,8 @@ import sys
 from scpi import SCPIDevice
 from scpi.transports.gpib import prologix
 from scpi.wrapper import AIOWrapper
+from scpi.devices.hp6632b import HP6632B
+from scpi.devices.generic import MultiMeter
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -24,10 +26,16 @@ if __name__ == "__main__":
     devlist = gpib.scan_devices()
     devdict = {}
     for addr, idstr in devlist:
+        man, model, _, _ = idstr.split(",")
         # Get device specific transport instance
         dtransport = aiogpib.get_device_transport(addr)
         # Get the device class with the transport
         aiodev = SCPIDevice(dtransport)
+        if man == "HEWLETT-PACKARD":
+            if model == "6632B":
+                aiodev = HP6632B(dtransport)
+            if model == "34401A":
+                aiodev = MultiMeter(dtransport)
         # And get the mapper that handles asyncio transparently
         devdict[addr] = AIOWrapper(aiodev)
         print("Added {:s} as devdict[{:d}]".format(idstr, addr))
